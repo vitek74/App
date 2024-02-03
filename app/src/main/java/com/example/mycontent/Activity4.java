625aa18f134f8df2ab95340cfcb2a78c925e1aa7
vitek74 <37906484+vitek74@users.noreply.github.com>
2024-02-03 12:50:41 +0300

    Второй коммит

--- app/src/main/java/com/example/mycontent/Activity4.java
+++ app/src/main/java/com/example/mycontent/Activity4.java
@@ -18,5 +18,27 @@ public class Activity4 extends AppCompatActivity {
             Intent intent = new Intent(Activity4.this, Activity4_1.class);
             startActivity(intent);
         });
+
+        // Переход к Activity4_2
+        Button buttonActivity4_2 = findViewById(R.id.buttonActivity4_2);
+        buttonActivity4_2.setOnClickListener(view -> {
+            Intent intent = new Intent(Activity4.this, Activity4_2.class);
+            startActivity(intent);
+        });
+
+        // Переход к Activity4_3
+        Button buttonActivity4_3 = findViewById(R.id.buttonActivity4_3);
+        buttonActivity4_3.setOnClickListener(view -> {
+            Intent intent = new Intent(Activity4.this, Activity4_3.class);
+            startActivity(intent);
+        });
+
+        // Переход к Activity4_4
+        Button buttonActivity4_4 = findViewById(R.id.buttonActivity4_4);
+        buttonActivity4_4.setOnClickListener(view -> {
+            Intent intent = new Intent(Activity4.this, Activity4_4.class);
+            startActivity(intent);
+        });
     }
 }
+
